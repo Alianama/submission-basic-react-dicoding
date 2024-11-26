45d1ee59e282159f40e64fb1b9ed3c67b367319a
Alipurnama <Alipurnama69@gmail.com>
2024-11-26 16:34:00 +0700

    feat(item) : add page item

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,18 +3,21 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return (
-      <div  >
-        <div className="" >
+      <div className="header-container">
+        <div className="title">
           <h1>APLIKASI CATATAN</h1>
         </div>
-        <div>
-          <form action="submit">
+        <div className="form-search-container">
+          <form className="input-form" action="submit">
             <input
               type="text"
               placeholder="search notes"
               name="search"
               id="search"
             />
+            <button className="submit-button" type="submit">
+              submit
+            </button>
           </form>
         </div>
       </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./components/header";
+import Item from "./components/item";
 
 // import style
 import "./styles/style.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <div className="main">
     <Header />
-  </>
+    <Item />
+  </div>
 );
